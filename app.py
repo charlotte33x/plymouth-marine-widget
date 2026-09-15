a913a462f0be9f7fbc62dbf726c7e8a3494ba1da
charlotte33x <charlotte.verney@yahoo.com>
2026-09-15 21:02:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -328,7 +328,7 @@ st.set_page_config(
     layout="wide"
 )
 st.markdown(
-    "<div style='color:#ff97b7;font-size:18px;font-weight:500;'>🌊 Plymouth Sea Conditions</div>",
+    "<div style='color:#ff97b7;font-size:18px;font-weight:500;'>Plymouth Sea Conditions</div>",
     unsafe_allow_html=True
 )
 import zoneinfo
@@ -341,7 +341,7 @@ st.caption(
     f"Updated {uk_time.strftime('%H:%M')}"
 )
 st.markdown(
-    "<div style='color:#ff97b7;font-size:18px;font-weight:500;'>🌡️ Temperature</div>",
+    "<div style='color:#ff97b7;font-size:18px;font-weight:500;'>Temperature</div>",
     unsafe_allow_html=True
 )
 st.markdown(
@@ -361,7 +361,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 st.markdown(
-    "<div style='color:#ff97b7;font-size:18px;font-weight:500;'>📅 3 Day Forecast</div>",
+    "<div style='color:#ff97b7;font-size:18px;font-weight:500;'>3 Day Forecast</div>",
     unsafe_allow_html=True
 )
 
@@ -378,7 +378,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 st.markdown(
-    "<div style='color:#ff97b7;font-size:18px;font-weight:500;'>🌊 Tides</div>",
+    "<div style='color:#ff97b7;font-size:18px;font-weight:500;'>Tides</div>",
     unsafe_allow_html=True
 )
 st.markdown(
@@ -399,7 +399,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 st.markdown(
-    "<div style='color:#ff97b7;font-size:18px;font-weight:500;'>🌙 Moon</div>",
+    "<div style='color:#ff97b7;font-size:18px;font-weight:500;'>Moon</div>",
     unsafe_allow_html=True
 )
 st.markdown(
